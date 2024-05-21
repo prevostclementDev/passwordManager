8610a449e52e84015c397efe61cfbe03c4e14fd7
lisatlt44 <lisa.tallet1@gmail.com>
2024-05-21 08:46:00 +0200

    tiens

---
+++ lib/model/password_model.dart
@@ -0,0 +1,28 @@
+import 'package:password_administrator/database/db_helper.dart';
+
+class Password {
+  final String site_name;
+  final String site_url;
+  final String password;
+
+  const Password({
+    required this.site_name,
+    required this.site_url,
+    required this.password,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'site_name': site_name,
+      'site_url': site_url,
+      'password': password
+    };
+  }
+
+  Future list() async {
+    final db = await DbHelper().getDatabase();
+    print( await db.query('passwords') );
+
+  }
+
+}
