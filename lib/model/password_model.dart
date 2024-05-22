3fd1c7255bdd132835ec4b86850aba90e774c2f3
prevostclementDev <clementprevost45@gmail.com>
2024-05-22 09:10:52 +0200

    correction visibility password

--- lib/model/password_model.dart
+++ lib/model/password_model.dart
@@ -5,12 +5,14 @@ class Password {
   final String site_url;
   final String password;
   final int id_user;
+  bool isPasswordVisible;
 
-  const Password({
+  Password({
     required this.site_name,
     required this.site_url,
     required this.password,
     required this.id_user,
+    this.isPasswordVisible = false,
   });
 
   Map<String, dynamic> toMap() {
@@ -27,5 +29,6 @@ class Password {
     site_url: map['site_url'],
     password: map['password'],
     id_user: map['id_user'],
+    isPasswordVisible : false
   );
 }
